663637cd8c3f6ac6d0da92030e394c0d07c8f01f
Yann Akoun <yannakoun@gmail.com>
2021-10-27 13:24:32 +0000

    initial commit

---
+++ starter.sh
@@ -0,0 +1,1 @@
+rails new $1 --database=postgresql --template https://github.com/techshelter/rails-starter-template/template.rb?raw=true
---
+++ template.rb
@@ -0,0 +1,12 @@
+generate(:migration, "enable_pg_crypto")
+rails_version = `rails -v`.split(' ').last.split('.')[0..1].join('.')
+f = Dir['.'][0]
+code = <<-CODE
+class EnablePgcrypto < ActiveRecord::Migration[#{rails_version}]
+  def change
+    enable_extension 'pgcrypto'
+  end
+end
+CODE
+file f, code
+
